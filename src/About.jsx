c5d380c29125c8050b42b6209436af462e395c61
Amadou-Marong <marongamadou6@gmail.com>
2024-12-24 13:18:09 +0000

    modified the about page

--- src/About.jsx
+++ src/About.jsx
@@ -4,36 +4,18 @@ import AnimatedSection from './Components/AnimatedSection'; // Import your anima
 const About = () => {
   return (
     <AnimatedSection>
-      <div
-        className="py-10 text-white h-auto max-w-[1400px] mx-auto bg-gradient-to-b from-gray-500 to-gray-800 left-1/2"
-        id="about"
-      >
-        <div className="flex sm:flex-row flex-col-reverse items-center justify-between md:gap-6 gap-12 px-10 mx-auto">
-          <div className="">
-            <div className="p-2">
-              <div className="text-gray-300 my-3 max-w-[500px]">
-                <h3 className="text-5xl text-justify leading-7 text-gray-200 font-semibold">
-                  About <span className="text-blue-400">Me</span>
-                </h3>
-                <p className="text-xl text-justify text-gray-400 my-8">
-                Hello! I'm Amadou Marong, a passionate Web Developer | Junior
-                Software Engineer with a knack for creating innovative
-                solutions. With a background in computer science, IT Support, I
-                have honed my skills in software engineering, frontend development using
-                in-demand technologies like Vue.js, React.js, and UX/UI design,
-                as well as some backend with Laravel. I've worked on a variety of exciting projects.
-              </p>
-              </div>
-            </div>
+      <div className="py-10 text-white max-w-[1400px] mx-auto bg-gradient-to-b from-gray-500 to-gray-800" id="about">
+        <div className="flex sm:flex-row flex-col-reverse items-center justify-between gap-6 px-10">
+          <div className="text-gray-300 max-w-[500px]">
+            <h3 className="text-5xl text-gray-200 font-semibold">
+              About <span className="text-blue-400">Me</span>
+            </h3>
+            <p className="text-xl text-gray-400 my-8">
+              Hi, I'm Amadou Marong, a Web Developer and Junior Software Engineer specializing in frontend development with Vue.js and React.js, with experience in UX/UI design and backend development with Laravel and Django.
+            </p>
           </div>
-          <div>
-            <div className="w-[500] h-full">
-              <img
-                src={aboutImg}
-                alt="about img"
-                className="object-cover rounded-full w-[40vw] backdrop-invert bg-white/30  object-top transition duration-200 hover:scale-110 cursor-pointer"
-              />
-            </div>
+          <div className="w-[500px]">
+            <img src={aboutImg} alt="about img" className="object-cover rounded-full w-[40vw] bg-white/30 transition duration-200 hover:scale-110 cursor-pointer" />
           </div>
         </div>
       </div>
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -11,127 +11,75 @@ import AnimatedSection from './Components/AnimatedSection'
 const Portfolio = () => {
     return (
       <AnimatedSection>
-      <div className='pt-16 pb-10 sm:pb-16 lg:gb-16 max-w-[1400] mx-auto] px-10' id='projects'>
+      <div className='pt-16 pb-10 sm:pb-16 lg:pb-16 max-w-[1400px] mx-auto px-10' id='projects'>
         <div className="grid md:grid-cols-2 mb-6">
-          <div>
-            <img src={work} className='w-[400px] mx-auto h-[300px]  object-cover object-top transition duration-200 hover:scale-110 rounded-lg cursor-pointer' alt="" />
-          </div>
-          <div className="text-gray-800 my-auto text-left">
-            <h3 className="text-5xl text-center leading-7 text-gray-800 font-semibold mb-8 tracking-tight mt-10">
-              My <span className="text-blue-400">Work</span>
-            </h3>
-            <p className="text-lg leading-relaxed max-w-[500px]">
-              Welcome to my portfolio! Here, you'll find a selection of my recent projects, 
-              showcasing my skills in design, development, and project management. Each project 
-              highlights my ability to deliver high-quality results and my commitment to continuous improvement.
-            </p>
-          </div>
+        <div>
+          <img src={work} className='w-[400px] mx-auto h-[300px] object-cover object-top transition duration-200 hover:scale-110 rounded-lg cursor-pointer' alt="Work" />
+        </div>
+        <div className="text-gray-800 my-auto text-left">
+          <h3 className="text-5xl text-center leading-7 text-gray-800 font-semibold mb-8 tracking-tight mt-10">
+          My <span className="text-blue-400">Work</span>
+          </h3>
+          <p className="text-lg leading-relaxed max-w-[500px]">
+          Welcome to my portfolio! Here, you'll find a selection of my recent projects, 
+          showcasing my skills in design, development, and project management. Each project 
+          highlights my ability to deliver high-quality results and my commitment to continuous improvement.
+          </p>
+        </div>
         </div>
 
         <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-6'>
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="" className="group col-span-3">
-              <img src={project1} alt="project 1" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Employee Management System</h2>
-              <p className="text-gray-400">
-                An employee management system with modules for managing leaves and tickets. This system helps in organizing and tracking employee information, leave requests, and IT support tickets efficiently.
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
+        {[{
+          img: project1,
+          title: 'Employee Management System',
+          description: 'An employee management system with modules for managing leaves and tickets. This system helps in organizing and tracking employee information, leave requests, and IT support tickets efficiently.',
+          github: '#',
+          live: '#'
+        }, {
+          img: project2,
+          title: 'Leave Management System',
+          description: 'A comprehensive leave management system that allows employees to apply for leaves, view leave balances, and track leave history. It also includes an approval workflow for managers to review and approve leave requests.',
+          github: '#',
+          live: 'https://ahmad-birthdays.netlify.app'
+        }, {
+          img: project3,
+          title: 'Ticketing Management System',
+          description: 'A ticketing management system for our IT Department to help manage IT-related issues. Test the app with username: admin and password: admin.',
+          github: '#',
+          live: 'https://ef7-studious-hubble.circumeo-apps.net/'
+        }, {
+          img: project4,
+          title: 'Project #4',
+          description: 'Lorem ipsum dolor sit amet.',
+          github: '#',
+          live: '#'
+        }, {
+          img: project5,
+          title: 'Project #5',
+          description: 'Lorem ipsum dolor sit amet.',
+          github: '#',
+          live: 'https://ahmad-food-menu.netlify.app'
+        }, {
+          img: project6,
+          title: 'Ecommerce Website',
+          description: 'An Ecommerce website for my web2 project work.',
+          github: '#',
+          live: 'https://ourhomepage.netlify.app'
+        }].map((project, index) => (
+          <div key={index} className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
+          <a href={project.live} className="group col-span-3">
+            <img src={project.img} alt={project.title} className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
+          </a>
+          <div className="p-4">
+            <h2 className='text-gray-200'>{project.title}</h2>
+            <p className="text-gray-400">{project.description}</p>
           </div>
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="" className="group col-span-3">
-              <img src={project2} alt="project 2" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Employee Management System</h2>
-              <p className="text-gray-400">
-                A comprehensive leave management system that allows employees to apply for leaves, view leave balances, and track leave history. It also includes an approval workflow for managers to review and approve leave requests.
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="https://ahmad-birthdays.netlify.app" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
+          <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
+            <a href={project.github} className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
+            <a href={project.live} className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
+          </div>  
           </div>
-          
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="https://ef7-studious-hubble.circumeo-apps.net/" className="group col-span-3">
-              <img src={project3} alt="project 1" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Ticketting Management System</h2>
-              <p className="text-gray-400">
-                a ticketting management system for a our IT Department to help and manage IT related issues
-                a have a test of the app login with username as admin and password: admin
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="https://ef7-studious-hubble.circumeo-apps.net/" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
-          </div>
-          
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="" className="group col-span-3">
-              <img src={project4} alt="project 4" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Project #4</h2>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet.
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="#" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
-          </div>
-          
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="https://ahmad-food-menu.netlify.app" className="group col-span-3">
-              <img src={project5} alt="project 5" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Project #5</h2>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet.
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="https://ahmad-food-menu.netlify.app" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
-          </div>
-          
-          <div className="rounded-xl bg-gradient-to-b from-gray-500 to-gray-800 overflow-hidden shadow-xl z-10">
-            <a href="https://ourhomepage.netlify.app" className="group col-span-3">
-              <img src={project6} alt="project 6" className='h-[200px] w-full object-cover object-top transition duration-200 group-hover:scale-110 rounded-lg'/>
-            </a>
-            <div className="p-4">
-              <h2 className='text-gray-200'>Ecomerce Website</h2>
-              <p className="text-gray-400">
-                an Ecommerce website for my web2 project work
-              </p>
-            </div>
-
-            <div className="flex flex-row gap-3 justify-end text-white mb-3 mr-3">
-              <a href="#" className="cursor-pointer"><AiFillGithub className='w-[35px] h-auto'/> </a>
-              <a href="https://ourhomepage.netlify.app" className="cursor-pointer"><AiFillChrome className='w-[35px] h-auto'/> </a>
-            </div>  
-          </div>
-
-
+        ))}
         </div> 
       </div>
       </AnimatedSection>
